1e3c75a9ebcf8026f6a189134c023441df3eb15b
Nancy Solanki <nnancy.ssolanki@gmail.com>
2024-01-08 22:02:09 +0530

    BookmyShow LLD

--- BookMyShowLLD.java
+++ BookMyShowLLD.java
@@ -1,49 +1,105 @@
-class BookingService{
+public class BookMyShow{
 
-    int tickets;
-    String cinema;
+    //API
+    public List<Cinema> getCinemas(String location , int date);
+    //API
+    public List<Movie> getMovie(String shows, String Location);
 
-    BookingService(int tickets , String cinema){
-        this.cinema=cinema;
-        this.tickets=tickets;
-    }
+}
+class Cinema{
+    String name;
+    String location;
+    List<Audi> audiList;
+   
+    //API
+    public Map<Date,Moviename> getMovies( String name, String location);
 
-    void bookTickets(){
-        System.out.println("Ticket Booked ");
-    }
-
-    int getTickets(){
-        return this.tickets;
-
-    }
-
-    String getCinema(){
-        return this.cinema;
-    }
+    public Map<Date,Moviename> getShows(int date);
 }
 
-class Payement{
-    int userID;
-    String paymentMethod;
+class Audi{
+    int audiId;
+    int totalSeats;
+    List<Shows> showsList;
 
-    enum StateOfPayemnt{
-        SUCCESS,FAILED,HOLD;
-    }
-
-    Payement(int userId, String paymentMethod){
-        this.userID=userId;
-        this.paymentMethod=paymentMethod;
-    }
-}
-class Notification{
-
-    void sendNotifications(){
-        System.out.println("Sent Notifications via whatsapp");
-    }
 }
 
-public class BookMyShowLLD {
-    public static void main(String[] args) {
-        
+class Shows{
+    int availableSeats;
+    List<Movies> moviesList;
+    List<Seats>seats;
+
+}
+
+class Seats{
+    Cinema cinema;
+    Status status;
+
+    //API
+    public Map<row,seatNo> availableSeats(Cinema cinema); 
+    
+    enum Status{
+        SUCCESS , RESERVED , EMPTY;
     }
+
+}
+
+class Movie{
+  String name;
+  String movieId;
+  String language;
+  String actor;
+
+}
+
+class User{
+    int userid;
+}
+
+class SystemUser extends User{
+    String name;
+    int AccountNumber;
+    String email;
+}
+
+class Customer extends SystemUser{
+    //API
+    public void makeBooking(){};
+    public void cancelBooking(){};
+
+}
+
+class Admin{
+    //APIs
+    public void addShows(){};
+    public void rescheduleShows(){};
+
+}
+class Search {
+
+	public List<Movie> searchMoviesByNames(String name);
+	public List<Movie> searchMoviesByGenre(Genre genre);
+
+}
+
+public class Booking {
+
+	String bookingId;
+	Customer customer;
+	Audi audi;
+	Shows show;
+	Seats.Status bookingStatus;
+	double totalAmount;
+	List<Seats> seats;
+	Payment paymentObj;
+
+	public boolean makePayment(Payment payment);
+
+}
+class Payment{
+    
+    int date;
+    int paymentId;
+    String mode;
+    
 }
