a3f315b56eb07a341b704d8a7907dc10f3527add
Nancy Solanki <nnancy.ssolanki@gmail.com>
2024-01-10 22:35:21 +0530

    Singleton pattern

---
+++ SingletonPattern.java
@@ -0,0 +1,27 @@
+
+class SingletonObject{
+       
+      //should have pvt constructor so that class cannot be instantiated
+       private SingletonObject(){} 
+       private static SingletonObject singleobj= new SingletonObject();
+
+       static SingletonObject getObject(){
+            return singleobj;
+       }
+
+       void helloMethod(){
+        System.out.println("Hey Guys, Hows life?");
+       }
+}
+//but this method doesn't work for multithreading as what if two threads try to create object at same time?
+// So we can lock that part of code that if one is accessing it other cannot
+
+public class SingletonPattern {
+    public static void main(String[] args) {
+        
+        SingletonObject object= SingletonObject.getObject();
+
+        object.helloMethod();
+
+    }
+}
